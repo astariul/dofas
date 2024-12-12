b708b8a32adaf1821e9e6e35c8416e7f911e9783
Astariul <remondnicola@gmail.com>
2024-12-12 10:24:19 +0100

    🔥 Remove dummy code

--- dofas/__init__.py
+++ dofas/__init__.py
@@ -1,13 +1,1 @@
-"""Dummy package content for `dofas`."""
-
-
-def is_odd(x: float) -> bool:
-    """Function checking if a number is odd.
-
-    Args:
-        x (float): Number to check.
-
-    Returns:
-        True if the number is odd, False otherwise.
-    """
-    return x % 2 == 1
+from .inspector import Inspector, extract_template_from
